fe28d246479549b71f25fe410f71c389af8119f1
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-11 23:39:28 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CostumeStores < ActiveRecord::Migration
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      t.timestamp
+      t.timestamps
     end
   end
 end
