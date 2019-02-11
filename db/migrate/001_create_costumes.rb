6259d7c41945f6df31a6c24fa4e78c77f4479e84
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-11 23:47:03 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -5,8 +5,6 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.float :price
       t.string :size 
       t.string :image_url
-      t.datetime :start_time
-      t.datetime :closing_time
     end
   end
 end
