483adde64e6473b42123b2bf0863786e0f9fc9f9
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-11 23:57:29 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,11 +3,11 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
-      t.integer :num_of_employees
-      t.boolean :still_in_business
-      t.timestamp :start_time
-      t.timestamp :end_time
+      # t.integer :costume_inventory
+      # t.integer :num_of_employees
+      # t.boolean :still_in_business
+      # t.timestamp :start_time
+      # t.timestamp :end_time
     end
   end
 end
