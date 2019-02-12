48ae96d5871db22cef76fa65477b3787e013614f
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-12 00:03:51 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.float :price 
       t.boolean :family_friendly
       t.timestamp :opening_date
+      t.timestamp :closing_date
     end
   end
 end
