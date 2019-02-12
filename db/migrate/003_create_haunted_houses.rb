b9247a65943463444a84227eb3e7eadcc0473c7b
Sheena Bonilla <sheena.myb@gmail.com>
2019-02-12 00:02:09 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,7 +1,9 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      t.string :name
+      t.string :name 
+      t.string :location 
+      t.
     end
   end
 end
